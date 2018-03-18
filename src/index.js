82c3b7694dd5930add7015e8c495272e265c8a8e
John Rees <johnrees@users.noreply.github.com>
2018-03-18 20:34:47 +0000

    github corner component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import classes from "./app.css";
 import { h, render } from 'preact';
 import Scene from './scene';
+import GithubCorner from './github_corner';
 
 render((
-	<Scene width={800} height={800} />
+	<div>
+		<Scene width={800} height={800} />
+		<GithubCorner repo="johnrees/powertochange-workshop" />
+	</div>
 ), document.getElementById('container'));
