7476763ed05a14ac65a7e226deb929b84e2a475e
John Rees <johnrees@users.noreply.github.com>
2018-03-18 21:23:02 +0000

    clicking and raycasting

--- src/scene.js
+++ src/scene.js
@@ -5,6 +5,21 @@ import { Observable } from "rxjs";
 import Model from "./model";
 import Ground from "./ground";
 
+function getPosition(x, y, width, height) {
+  return [x / width * 2 - 1, -(y / height) * 2 + 1];
+}
+
+function checkForIntersection(event) {
+  const [x, y] = getPosition(
+    event.offsetX,
+    event.offsetY,
+    this.props.width,
+    this.props.height
+  );
+  this.raycaster.setFromCamera({x, y}, this.camera);
+  return this.raycaster.intersectObject(this.model);
+}
+
 export default class Scene extends Component {
 
   constructor(props) {
@@ -22,21 +37,33 @@ export default class Scene extends Component {
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(width, height);
 
+    this.raycaster = new THREE.Raycaster();
+
+    this.model = new Model();
+
     this.render3D = this.render3D.bind(this);
   }
 
   componentDidMount() {
     this.scene.add(Ground());
-    this.scene.add(new Model());
+    this.scene.add(this.model);
 
     this.controls = new OrbitControls(this.camera);
     this.controls.maxPolarAngle = 1.5;
 
     const wheel$ = Observable
-      .fromEvent(this.renderer.domElement, 'wheel')
+      .fromEvent(this.renderer.domElement, 'wheel');
 
     const mouseMove$ = Observable
       .fromEvent(this.renderer.domElement, 'mousemove')
+      // .share();
+
+    const mouseDown$ = Observable
+      .fromEvent(this.renderer.domElement, 'mousedown')
+
+    mouseDown$
+      .map(checkForIntersection.bind(this))
+      .subscribe(console.log);
 
     this.render$ = Observable.merge(wheel$, mouseMove$)
                       .startWith(true)
