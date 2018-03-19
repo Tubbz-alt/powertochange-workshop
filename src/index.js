98818a0f69121471b668e74b823293c5d173f953
John Rees <johnrees@users.noreply.github.com>
2018-03-19 14:27:05 +0000

    reload page on save

--- src/index.js
+++ src/index.js
@@ -11,3 +11,20 @@ render((
 		<Stats />
 	</div>
 ), document.getElementById('container'));
+
+if (module.hot) {
+  module.hot.dispose(function () {
+		// remove WebGL context then force reload the page
+		var canvas = document.querySelector('canvas');
+		var gl = canvas.getContext('webgl');
+		canvas.addEventListener('webglcontextlost', function(e) {
+			console.clear();
+			window.location.reload();
+		}, false);
+		gl.getExtension('WEBGL_lose_context').loseContext();
+  });
+
+  module.hot.accept(function () {
+    // module or one of its dependencies was just updated
+  });
+}
